230d56e7b67f2cc51448db385d23f84896a93fb3
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-25 15:05:50 +0800

    refactor: update import path using the alias @

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import Navbar from "@/layouts/components/Navbar";
+import Sidebar from "@/layouts/components/Sidebar";
 
 const RootLayout = () => {
   return (
