31cdb5f861539fe4e3325d783835a54604b21c32
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-25 15:42:44 +0800

    feat: Create UI UX for RootLayout component positioned, Sidebar and Navbar

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,11 +4,23 @@ import Sidebar from "@/layouts/components/Sidebar";
 
 const RootLayout = () => {
   return (
-    <div>
-      <h1>Root Layout</h1>
-      <Navbar />
-      <Sidebar />
-      <Outlet />
+    <div className="layout-wrapper layout-content-navbar">
+      <div className="layout-container">
+        <Sidebar />
+        <div className="layout-page">
+          <Navbar />
+          <div className="content-wrapper">
+            <div className="flex-grow-1 container-p-y container-fluid my-0">
+              <Outlet />
+            </div>
+            {/* Footer 
+         Footer */}
+            <div className="content-backdrop fade"></div>
+          </div>
+        </div>
+      </div>
+      <div className="layout-overlay layout-menu-toggle"></div>
+      <div className="drag-target"></div>
     </div>
   );
 };
