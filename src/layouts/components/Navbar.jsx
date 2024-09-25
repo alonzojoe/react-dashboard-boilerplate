9712c67532450473336c833a7fba9bdc3a0029f5
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-25 15:40:13 +0800

    feat: styled navbar and sidebar layout components

--- src/layouts/components/Navbar.jsx
+++ src/layouts/components/Navbar.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import Profile from "@/assets/images/default-profile.jpeg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav
+      className="m-0 rounded-0 w-auto layout-navbar navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme container-fluid"
+      id="layout-navbar"
+    >
+      <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+        <a className="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+          <i className="ti ti-menu-2 ti-sm"></i>
+        </a>
+      </div>
+
+      <div
+        className="navbar-nav-right d-flex align-items-center"
+        id="navbar-collapse"
+      >
+        <div className="navbar-nav align-items-center">
+          <div className="nav-item navbar-search-wrapper mb-2">
+            <h4 className="mt-4 fw-bold">Dashboard</h4>
+          </div>
+        </div>
+
+        <ul className="navbar-nav flex-row align-items-center ms-auto">
+          <li className="nav-item dropdown-language dropdown me-2 me-xl-0">
+            <a className="nav-link hide-arrow" href="javascript:void(0);">
+              <i className="ti rounded-circle ti-md"></i>
+            </a>
+          </li>
+
+          <li
+            className="nav-item dropdown-language dropdown me-2 me-xl-0"
+            id="user-name"
+          >
+            <a className="nav-link hide-arrow" href="javascript:void(0);">
+              <h5 className="mt-3 fw-semibold">Joenell Alonzo</h5>
+            </a>
+          </li>
+
+          <li className="nav-item navbar-dropdown dropdown-user">
+            <a className="nav-link hide-arrow" href="javascript:void(0);">
+              <div className="avatar avatar-online">
+                <img
+                  src={Profile}
+                  alt="user-avatar"
+                  className="h-auto rounded-circle"
+                />
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- src/layouts/components/Sidebar.jsx
+++ src/layouts/components/Sidebar.jsx
@@ -1,8 +1,90 @@
+import Logo from "@/assets/images/react-logo.png";
+
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <aside
+      id="layout-menu"
+      className="layout-menu menu-vertical bg-menu-theme"
+      style={{
+        touchAction: "none",
+        userSelect: "none",
+        WebkitUserDrag: "none",
+        WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
+      }}
+    >
+      <div className="app-brand demo py-1 d-flex align-items-center">
+        <a className="app-brand-link">
+          <span className="app-brand-logo demo">
+            {/* <img
+              src={Logo}
+              className="img-fluid"
+              height="60"
+              width="60"
+              alt="app-logo"
+            /> */}
+          </span>
+          <span className="app-brand-text demo menu-text fw-bold">
+            App Name
+          </span>
+        </a>
+        <a
+          href="javascript:void(0);"
+          className="layout-menu-toggle menu-link text-large ms-auto mb-2"
+        >
+          <div className="demo-vertical-spacing btn-switch">
+            <div className="has-error">
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  className="switch-input switch-input"
+                  true-value={true}
+                  false-value={false}
+                />
+                <span className="switch-toggle-slider">
+                  <span className="switch-on"></span>
+                  <span className="switch-off"></span>
+                </span>
+                <span className="switch-label"></span>
+              </label>
+            </div>
+          </div>
+          <i className="ti ti-x d-block d-xl-none ti-sm align-middle btn-x"></i>
+        </a>
+      </div>
+      <div className="menu-inner-shadow"></div>
+      <ul className="menu-inner py-1">
+        <li className="menu-header small text-uppercase">
+          <span className="menu-header-text">Home</span>
+        </li>
+        <li className="menu-item active">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons ti ti-dashboard"></i>
+            <div>Dashboard</div>
+          </a>
+        </li>
+        <li className="menu-item">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons ti ti-home"></i>
+            <div>Home</div>
+          </a>
+        </li>
+        <li className="menu-item">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons ti ti-file"></i>
+            <div>Sample Page</div>
+          </a>
+        </li>
+        <li className="menu-header small text-uppercase">
+          <span className="menu-header-text">Auth</span>
+        </li>
+        <li className="menu-item">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons ti ti-logout"></i>
+            <div data-i18n="logout">Logout</div>
+          </a>
+        </li>
+      </ul>
+    </aside>
   );
 };
 
