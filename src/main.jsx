cc7e5d36a532e98959c5819553fafc65507d639b
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-25 15:11:53 +0800

    chore:  import assets(incl, icons,css)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+//icons
+import "@/assets/fonts/fontawesome.css";
+import "@/assets/fonts/tabler-icons.css";
+import "@/assets/fonts/flag-icons.css";
+
+//core css
+import "@/assets/css/rtl/core.css";
+import "@/assets/css/rtl/theme-default.css";
+import "@/assets/css/demo.css";
+import "@/assets/fonts/flag-icons.css";
+import "@/assets/css/rtl/theme-semi-dark.css";
+
+//pages
+import "@/assets/css/pages/cards-advance.css";
+import "@/assets/css/rtl/custom-style.css";
+import "@/assets/js/app.js";
+
+//custom-css
+import "@/assets/css/custom/custom.css";
+
+//spinner
+import "@/assets/libs/spinkit/spinkit.css";
+import "@/index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
