dbf2b9cb72f9998c0286ef74e1692629e5b0dbe0
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-26 14:35:52 +0800

    feat: setup ui initialState layout, create reducer that sets the layout value, create helper fn localStorage actions for non React components

---
+++ src/lib/utils.js
@@ -0,0 +1,17 @@
+export const setStorageItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getStorageItem = (key) => {
+  const storedValue = localStorage.getItem(key);
+
+  return storedValue ? JSON.parse(storedValue) : undefined;
+};
+
+export const removeStorageItem = (key) => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/store/slices/ui-slice.js
+++ src/store/slices/ui-slice.js
@@ -1,8 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getStorageItem, setStorageItem } from "@/lib/utils";
+
+const storedLayout = getStorageItem("layout");
+
+const initialState = {
+  layout: storedLayout ? storedLayout : "layout-menu",
+};
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: {},
+  initialState,
+  reducers: {
+    setLayout(state, action) {
+      const htmlEl = document.querySelector("html");
+
+      if (action.payload === "layout-menu") {
+        htmlEl.classList.remove("layout-menu");
+        htmlEl.classList.add("layout-menu-collapsed");
+      } else {
+        htmlEl.classList.remove("layout-menulayout-menu-collapsed");
+        htmlEl.classList.add("layout-menu");
+      }
+      setStorageItem("layout", action.payload);
+      state.layout = action.payload;
+    },
+  },
 });
 
 export default uiSlice.reducer;
