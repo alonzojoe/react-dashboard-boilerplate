741757f54704e9a47f43eda47de77dacb9d81b81
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-26 11:23:33 +0800

    feat: setup redux store, create uiSlice and export it  as reducer and place item in configureStore, wrap App using Provider with store = exported configureStore

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
+import store from "./store/index.js";
 
 //icons
 import "@/assets/fonts/fontawesome.css";
@@ -28,6 +30,8 @@ import "@/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
